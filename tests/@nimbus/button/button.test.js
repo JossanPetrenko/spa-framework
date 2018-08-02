bba773d740b85b382933ce839214d929bd3b0652
Jossan Petrenko <jossanpetrenko@hotmail.com>
2018-08-02 18:48:42 -0400

    Configuração chai-jest-snapshot

--- tests/@nimbus/button/button.test.js
+++ tests/@nimbus/button/button.test.js
@@ -6,9 +6,28 @@ import Button from '../../../packages/@nimbus/button';
 
 describe('<Button />', () => {
 
-  it('renders the content', () => {
+  it('Renderiza o conteúdo', () => {
     const wrapper = render(<Button> Texto Simples </Button>);
     expect(wrapper.text()).to.contain(' Texto Simples ');
   });
+
+  it('should render a label', () => {
+    const wrapper = shallow(
+        <Button>Hello Jest!!!!!!</Button>
+    );
+    expect(wrapper).to.matchSnapshot();
+});
+it('should render a small label', () => {
+    const wrapper = shallow(
+        <Button small>Hello Jest!!!</Button>
+    );
+    expect(wrapper).to.matchSnapshot();
+});
+it('should render a grayish label', () => {
+    const wrapper = shallow(
+        <Button light>Hello Jest!!</Button>
+    );
+    expect(wrapper).to.matchSnapshot();
+});
 
 });
--- tests/setupTests.js
+++ tests/setupTests.js
@@ -1,4 +1,21 @@
 import Enzyme from 'enzyme';
+import { shallow, render, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import chai from "chai";
+import chaiJestSnapshot from "chai-jest-snapshot";
+
+chai.use(chaiJestSnapshot);
+
+global.shallow = shallow;
+global.render = render;
+global.mount = mount;
+
+// Pula as mensagens de aviso do 'createElement'
+// Mas retorna um erro para qualquer outra
+console.error = message => {
+  if (!/(React.createElement: type should not be null)/.test(message)) {
+      throw new Error(message);
+  }
+};
 
 Enzyme.configure({ adapter: new Adapter() });
