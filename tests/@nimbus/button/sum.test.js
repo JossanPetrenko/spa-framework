340046d294dd1e27f61726052a621671fd42c296
Jossan Petrenko <jossanpetrenko@hotmail.com>
2018-08-03 11:10:43 -0400

    Sinon

--- tests/@nimbus/button/sum.test.js
+++ tests/@nimbus/button/sum.test.js
@@ -1,13 +1,13 @@
  import sum from './sum';
  import {expect}  from 'chai';
- import {stub} from 'sinon';
+ import sinon from 'sinon';
 
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).to.equal(3);
 });
 
 test('adds 1 + 2 to equal 3 com callback', () => {
-  const callback = stub();
+  const callback = sinon.fake();
   expect(sum(1, 2, callback)).to.equal(3);
   expect(callback.getCall(0).args[0]).to.equal(3);
 });
