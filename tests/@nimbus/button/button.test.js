40b219ea1a97463423c1fe40fd2fc40a264fd2a1
Jossan Petrenko <jossanpetrenko@hotmail.com>
2018-08-08 16:58:06 -0400

    Button - Intents - Tests

--- tests/@nimbus/button/button.test.js
+++ tests/@nimbus/button/button.test.js
@@ -8,7 +8,7 @@ import Button from '../../../packages/@nimbus/button';
 
 describe('<Button />', () => {
 
-  it('deve renderizar o botão padrão', () => {
+  it('deve renderizar o botão default', () => {
     const wrapper = shallow(
         <Button>{t('common.padrao')}</Button>
     );
@@ -16,7 +16,7 @@ describe('<Button />', () => {
     expect(wrapper.text()).to.contain(t('common.padrao'));
   });
 
-  it('deve renderizar o botão desabilitado', () => {
+  it('deve renderizar o botão disabled', () => {
     const wrapper = shallow(
         <Button disabled>{t('common.desabilitado')}</Button>
     );
@@ -24,7 +24,7 @@ describe('<Button />', () => {
     expect(wrapper.text()).to.contain(t('common.desabilitado'));
   });
 
-  it('deve renderizar o botão primario', () => {
+  it('deve renderizar o botão primary', () => {
     const wrapper = shallow(
         <Button primary>{t('common.primario')}</Button>
     );
@@ -32,12 +32,44 @@ describe('<Button />', () => {
     expect(wrapper.text()).to.contain(t('common.primario'));
   });
 
-  it('deve renderizar o botão secundario', () => {
+  it('deve renderizar o botão secundary', () => {
     const wrapper = shallow(
         <Button secondary>{t('common.secundario')}</Button>
     );
     expect(wrapper).to.matchSnapshot();
     expect(wrapper.text()).to.contain(t('common.secundario'));
   });
+
+  it('deve renderizar o botão info', () => {
+    const wrapper = shallow(
+        <Button info>{t('common.informacoes')}</Button>
+    );
+    expect(wrapper).to.matchSnapshot();
+    expect(wrapper.text()).to.contain(t('common.informacoes'));
+  });
+
+  it('deve renderizar o botão warning', () => {
+    const wrapper = shallow(
+        <Button warning>{t('common.alerta')}</Button>
+    );
+    expect(wrapper).to.matchSnapshot();
+    expect(wrapper.text()).to.contain(t('common.alerta'));
+  })
+
+  it('deve renderizar o botão success', () => {
+    const wrapper = shallow(
+        <Button success>{t('common.sucesso')}</Button>
+    );
+    expect(wrapper).to.matchSnapshot();
+    expect(wrapper.text()).to.contain(t('common.sucesso'));
+  })
+
+  it('deve renderizar o botão danger', () => {
+    const wrapper = shallow(
+        <Button danger>{t('common.perigo')}</Button>
+    );
+    expect(wrapper).to.matchSnapshot();
+    expect(wrapper.text()).to.contain(t('common.perigo'));
+  })
 
 });
