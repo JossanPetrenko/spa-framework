d64230aa7e1ca7a9898f36cf948f87d3ac1700a5
Jossan Petrenko <jossanpetrenko@hotmail.com>
2018-08-08 21:53:09 -0400

    Strings

--- packages/@nimbus/strings/common.strings.js
+++ packages/@nimbus/strings/common.strings.js
@@ -5,18 +5,56 @@ export default extend({
 
     common: {
         //keep alphabetical order
+        'abrir_filtro': 'Abrir Filtro',
         'alerta': 'Alerta',
+        'aplicar': 'Aplicar',
+        'aplicar_filtros': 'Aplicar Filtros',
+        'buscar': 'Buscar',
         'button': 'BUTTON',
+        'cadastros': 'Cadastros',
+        'cancelar': 'Cancelar',
+        'configuracoes': 'Configuraçōes',
         'desabilitado': 'Desabilitado',
+        'detalhes': 'Detalhes',
+        'editar': 'Editar',
         'erro': 'Erro',
+        'exportar': 'Exportar',
+        'fechar': 'Fechar',
+        'fechar_filtro': 'Fechar Filtro',
+        'filtrar': 'Filtrar',
+        'filtros': 'Filtros',
         'flat': 'Flat',
+        'home': 'Home',
         'icone': 'Ícone',
         'informacoes': 'Informações',
+        'invertido': 'Invertido',
+        'limpar': 'Limpar',
+        'limpar_filtros': 'Limpar Filtros',
+        'nao_e_permitido_acao_botao': 'Sem permissão para esta ação',
+        'não': 'Não',
+        'ok': 'Ok',
         'outline': 'Outline',
         'padrao': 'Padrão',
         'perigo': 'Perigo',
         'primario': 'Primário',
+        'salvar': 'Salvar',
+        'salvo_com_sucesso': 'Salvo com sucesso!',
         'secundario': 'Secundário',
+        'selecione': 'Selecione',
+        'sim': 'Sim',
         'sucesso': 'Sucesso',
+        'tabela_vazia': 'Não há itens a serem exibidos',
+        'visualizar': 'Visualizar',
+        'voltar': 'Voltar',
+
+        'excluir': 'excluir',
+        'remover': 'Remover',
+        'deseja_realmente_remover': 'Deseja realmente remover?',
+
+        'novo': 'Novo',
+        'adicionar': 'Adicionar',
+
+
+
     }
 });
---
+++ packages/@nimbus/strings/common_business.js
@@ -0,0 +1,10 @@
+import { extend } from './polyglot';
+
+//prettier-ignore
+export default extend({
+
+    common_business: {
+        //keep alphabetical order
+        //linguagem ubíqua(vocabulário do negócio)
+    }
+});
