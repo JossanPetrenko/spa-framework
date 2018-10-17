939eb69c6969aa07cc1ddab52f3776b992017091
castrolol <lu4n.k4str0@gmail.com>
2018-10-17 04:50:38 -0400

    Build docs

--- doczrc.js
+++ doczrc.js
@@ -2,7 +2,7 @@ import { css } from 'docz-plugin-css';
 import doczPluginGithubPage from 'docz-plugin-github-page';
 
 export default {
-	plugins: [ css({ preprocessor: 'sass' }) ],
+	plugins: [ css({ preprocessor: 'sass' }), doczPluginGithubPage() ],
 	title: 'Nimbus',
 	description: 'Nimbus - SPA Framework',
 	indexHtml: '.docz/index_template.html'
